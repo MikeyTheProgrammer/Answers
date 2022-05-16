d9f47536bae28a1ad979748810cd16783f4b0152
MikeyTheProgrammer <mikiamin18@gmail.com>
2022-05-16 09:11:46 +0300

    13ex

--- 13ex.js
+++ 13ex.js
@@ -1,9 +1,11 @@
-console.log("*")
-console.log("* *")
-console.log("* * *")
-console.log("* * * *")
-console.log("* * * * *")
-console.log("* * * *")
-console.log("* * *")
-console.log("* *")
-console.log("*")
+let star = "*"
+for (index = 0; index <= 4; index++){
+console.log(star)
+star += "*"
+}
+
+star = star.slice(0, -1);
+
+for (index = 0; index <= 4; index++){
+    star = star.slice(0, -1);
+    console.log(star) }
